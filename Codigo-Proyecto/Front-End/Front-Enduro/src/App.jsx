b396574d17da3cc8fc3d1606b2a9a1bf8d9e84c5
Andres Jaimes <andresrevolledo2004@gmail.com>
2025-05-13 10:26:38 -0500

    Modificando componente loginform y modificando index

--- Codigo-Proyecto/Front-End/Front-Enduro/src/App.jsx
+++ Codigo-Proyecto/Front-End/Front-Enduro/src/App.jsx
@@ -3,7 +3,7 @@ import Login from "./features/auth/pages/Login";
 function App() {
   return (
     <>
-      <div className="overflow-hidden w-full h-screen bg-[#EFEFEF]">
+      <div className="overflow-hidden w-full h-screen bg-[#e6e6e6]">
         <Login />
       </div>
     </>
--- Codigo-Proyecto/Front-End/Front-Enduro/src/features/auth/components/LoginForm.jsx
+++ Codigo-Proyecto/Front-End/Front-Enduro/src/features/auth/components/LoginForm.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 const LoginForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
   return (
     <div
       className="flex w-[820px] h-[400px] max-md:w-full max-md:h-[90%] justify-center items-center
-     bg-[#AFCBD3] rounded-[20px] gap-5 p-20 max-md:!flex-col max-md:p-10"
+     bg-[#AFCBD3] rounded-[20px] gap-5 p-20 max-md:!flex-col max-md:p-10 max-sm:items-start max-sm:gap-2"
     >
       <div className="flex flex-col w-[50%] justify-center items-center gap-10 max-md:w-full">
         <div className="flex flex-col justify-center items-center gap-4 max-md:w-full">
@@ -14,15 +21,36 @@ const LoginForm = () => {
             placeholder="USERNAME"
             className="text-center text-xl text-black bg-white rounded-2xl w-full h-[45px]"
           />
-          <input
-            type="password"
-            placeholder="●●●●●●"
-            className="text-center text-4xl text-black bg-white rounded-2xl w-full h-[45px]"
-          />
+          <div className="relative w-full">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="●●●●●●●"
+              autoComplete="new-password"
+              inputMode="none"
+              className="text-center text-xl text-black bg-white rounded-2xl w-full h-[45px] password-input"
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+            />
+            {isFocused && (
+              <button
+                type="button"
+                onMouseDown={(e) => e.preventDefault()}
+                onClick={togglePasswordVisibility}
+                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 "
+                style={{ pointerEvents: "auto" }}
+              >
+                {showPassword ? (
+                  <AiOutlineEyeInvisible size={20} />
+                ) : (
+                  <AiOutlineEye size={20} />
+                )}
+              </button>
+            )}
+          </div>
+          <button className="bg-[#FF7700] cursor-pointer text-white items-center justify-center mt-6 w-full h-[45px] rounded-2xl flex">
+            <a href="#">I N G R E S A R</a>
+          </button>
         </div>
-        <button className="bg-[#FF7700] cursor-pointer text-white items-center justify-center w-full h-[45px] rounded-2xl flex">
-          <a href="#">I N G R E S A R</a>
-        </button>
       </div>
       <div className="flex flex-col w-[50%] justify-center items-center max-md:w-full">
         <img
